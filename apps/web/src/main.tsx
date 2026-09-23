48a5d09a80b7540fc4e5a5409b9df78ffcfa075d
Comrade Lemoncake <jdbohrman@outlook.com>
2026-09-23 18:32:59 -0400

    Load Entra App Registration branding

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,8 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App.js'
+import { hydrateEntraAppBranding } from './entra-branding.js'
 import './styles.css'
 import './identity-branding.css'
+import './entra-identity.css'
 import './classification.css'
+
+void hydrateEntraAppBranding()
 
 createRoot(document.getElementById('root') as HTMLElement).render(<StrictMode><App /></StrictMode>)

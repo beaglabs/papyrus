41a2de6b21b242c7566bbb7e600f2557f5898a54
Comrade Lemoncake <jdbohrman@outlook.com>
2026-09-24 23:47:39 -0400

    Use Papyrus logo as favicon

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,9 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App.js'
+import papyrusLogo from '../../../deploy/marketplace/logos/papyrus-small-48x48.png'
 import './styles.css'
 import './identity-branding.css'
 import './classification.css'
 import './portal-layout.css'
+
+const favicon = document.querySelector<HTMLLinkElement>('link[rel="icon"]') ?? document.createElement('link')
+favicon.rel = 'icon'
+favicon.type = 'image/png'
+favicon.href = papyrusLogo
+if (!favicon.isConnected) document.head.appendChild(favicon)
 
 createRoot(document.getElementById('root') as HTMLElement).render(<StrictMode><App /></StrictMode>)
